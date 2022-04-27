ca2736c3324d7c47a3d81ca0882fcccb2bcfbbfe
aliaksandrnatashkin <natashkinsasha@gmail.com>
2022-04-27 13:21:57 +0400

    update test4.js

--- test4.js
+++ test4.js
@@ -1,3 +1,19 @@
 //Re-write test2.js class in order to resolve any issues, and comment everything in order to let VSCode help you suggesting the code and its meaning
 
-// There are no requirements, it is not clear what needs to be rewritten. And I don't use VSCode.
+class MyClass {
+    constructor(firstName, lastName) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        Object.freeze(this);
+    }
+
+    getFullName() {
+        return `${this._firstName} ${this._lastName}`;
+    }
+}
+
+class MyClass2 {
+    static getFullName(entity) {
+        return entity.getFullName();
+    }
+}
